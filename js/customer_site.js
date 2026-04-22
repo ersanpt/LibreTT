8860aa14b967aa7f1f84a80a621afbc94a887371
ersanpt <143370274+ersanpt@users.noreply.github.com>
2026-04-22 10:35:18 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -2,34 +2,6 @@ const CUSTOMER_SITES = {
     lsbzy: {
         api: 'https://apilsbzy1.com/api.php/provide/vod',
         name: '老色逼资源',
-    },
-    fhzy: {
-        api: 'http://fhapi9.com/api.php/provide/vod',
-        name: '番号资源',
-    },
-    nxxzy: {
-        api: 'https://naixxzy.com/api.php/provide/vod',
-        name: '奶香香资源',
-    },
-    ddzy: {
-        api: 'https://api.ddapi.cc/api.php/provide/vod',
-        name: '滴滴资源',
-    },
-    ytzy: {
-        api: 'https://apiyutu.com/api.php/provide/vod',
-        name: '玉兔资源',
-    },
-    aizy: {
-        api: 'http://lbapiby.com/api.php/provide/vod/at/json',
-        name: 'AI资源',
-    },
-    115zy: {
-        api: 'https://155api.com/api.php/provide/vod',
-        name: '155直连',
-    },
-    lbzy: {
-        api: 'https://lbapi9.com/api.php/provide/vod',
-        name: '乐播直连',
     }
 };
 
