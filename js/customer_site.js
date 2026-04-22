0b1610f6d6c01f47f35edfc26d0ccedea6ebd915
ersanpt <143370274+ersanpt@users.noreply.github.com>
2026-04-22 10:48:57 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -91,37 +91,37 @@ const CUSTOMER_SITES = {
         api: 'https://cj.lziapi.com/api.php/provide/vod/',
         name: '量子资源站'
     },
-    lsbzy: {
+    crlsbzy: {
         api: 'https://apilsbzy1.com/api.php/provide/vod',
         name: '老色逼资源',
     },
-    fhzy: {
+    crfhzy: {
         api: 'https://fhapi9.com/api.php/provide/vod',
         name: '番号资源',
     },
-    nxxzy: {
+    crnxxzy: {
         api: 'https://naixxzy.com/api.php/provide/vod',
         name: '奶香香资源',
     },
-    ddzy: {
+    crddzy: {
         api: 'https://api.ddapi.cc/api.php/provide/vod',
         name: '滴滴资源',
     },
-    ytzy: {
+    crytzy: {
         api: 'https://apiyutu.com/api.php/provide/vod',
         name: '玉兔资源',
     },
-    aizy: {
-        api: 'https://lbapiby.com/api.php/provide/vod/at/json',
+    craizy: {
+        api: 'https://lbapiby.com/api.php/provide/vod',
         name: 'AI资源',
     },
-    115zy: {
+    cr115zy: {
         api: 'https://155api.com/api.php/provide/vod',
-        name: '155直连',
+        name: '155资源',
     },
-    lbzy: {
+    crlbzy: {
         api: 'https://lbapi9.com/api.php/provide/vod',
-        name: '乐播直连',
+        name: '乐播资源',
     },
 };
 
